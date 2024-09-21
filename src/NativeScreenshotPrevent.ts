9a7d08551232c88a1d4fe529a2d2c4eb017adef3
Batuhan Öztürk <batuhanoztrk@hotmail.com>
2024-09-21 20:32:43 +0300

    refactor: remove unused addListener method

--- src/NativeScreenshotPrevent.ts
+++ src/NativeScreenshotPrevent.ts
@@ -5,7 +5,6 @@ export interface Spec extends TurboModule {
   enabled(value: boolean): void;
   enableSecureView(imagePath?: string): void;
   disableSecureView(): void;
-  addListener(fn: Function): void;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('ScreenshotPrevent');
